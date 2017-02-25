8efc5e7774cf877a2629145867ef8e5f14611465
Zachary Splingaire <splinga2@illinois.edu>
2017-02-25 17:33:35 -0600

    Implemented first draft of graph. Added test file.

---
+++ Scheduler/src/main/java/org/apache/airavata/graph/graphTest.java
@@ -0,0 +1,42 @@
+package org.apache.airavata.graph;
+
+import org.apache.airavata.graph.DiGraph;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class graphTest {
+	public static void main(String [] args) {
+		DiGraph graph = new DiGraph();
+		
+		graph.addVertex(-1);
+		graph.addVertex(-2);
+		for(int i = 1; i < 9; i++) {
+			graph.addVertex(i);
+		}
+		
+		for(int i = 1; i < 5; i++) {
+			graph.addEdge(-1, i);
+		}
+		
+		for(int i = 5; i < 9; i++) {
+			graph.addEdge(i, -2);
+		}
+		
+		graph.addEdge(1, 6);
+		graph.addEdge(1, 8);
+		graph.addEdge(2, 5);
+		graph.addEdge(2, 6);
+		graph.addEdge(3, 6);
+		graph.addEdge(3, 7);
+		graph.addEdge(4, 8);
+		
+		HashSet<Edge> edges = graph.maxFlow(-1, -2);
+		
+		Iterator<Edge> it = edges.iterator();
+		while(it.hasNext()) {
+			Edge e = it.next();
+			if(e.job != -1 && e.worker != -2)
+				System.out.printf("Edge from %d to %d\n", e.job, e.worker);
+		}
+	}
+}
