961a2a18bf67f6e2204631c2a769c05da8534eef
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2017-02-25 11:15:22 -0600

    Removed GPL'ed library, Added the graph stub code and the pseudocode for the load balancer

---
+++ Scheduler/src/main/java/org/apache/airavata/graph/DiGraph.java
@@ -0,0 +1,44 @@
+package org.apache.airavata.graph;
+
+import java.util.HashSet;
+
+public class DiGraph {
+	
+	private class Edge{
+		int job;
+		int worker;
+		boolean isSOrT;
+	}
+	
+	public DiGraph(){
+		
+	}
+	
+	public void addVertex(int vertex){
+		
+	}
+	
+	public void addEdge(int vertex1, int vertex2){
+		
+	}
+	
+	public void removeVertex(int vertex){
+		
+	}
+	
+	public void removeEdge(int vertex1, int vertex2){
+		
+	}
+	
+	public HashSet<Edge> maxFlow(int sink, int target){
+		return null;
+	}
+	
+	public int numVertexes(){
+		return 0;
+	}
+	
+	public int numEdges(){
+		return 0;
+	}
+}
